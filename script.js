efaf0063b2b7c773dbb85eb9fc1abdcc85e8ba48
João Paulo Amendola <joaoa@id.uff.br>
2022-08-10 14:23:02 -0300

    versao 1.0

---
+++ script.js
@@ -0,0 +1,9 @@
+let bebida;
+
+function escolher_bebida(){
+    document.getElementById("coca").style.borderColor ="#26ff00";
+    document.getElementById("coca").style.borderWidth ="2px";
+    document.getElementById("coca").style.borderStyle ="solid";
+    bebida = "coca";
+    let valor = 4.90
+}
